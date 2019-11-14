7ef5b2e39f6eecd4f8dc8d9be64c8880c223903d
SawyerClick <src2188@columbia.edu>
2019-11-13 21:07:47 -0500

    d3-force chart and updated scrollytelling css

--- src/scripts/chart-01.js
+++ src/scripts/chart-01.js
@@ -1,13 +1,14 @@
 import * as d3 from 'd3'
+// import Force from 'd3-force'
 import d3Tip from 'd3-tip'
 import d3Annotation from 'd3-svg-annotation'
 d3.tip = d3Tip
 
 let audio = null
 
-const margin = { top: 20, left: 130, right: 30, bottom: 30 }
-const height = 270 - margin.top - margin.bottom
-const width = 1100 - margin.left - margin.right
+const margin = { top: 50, left: 130, right: 130, bottom: 40 }
+const height = 600 - margin.top - margin.bottom
+const width = 750 - margin.left - margin.right
 
 const svg = d3
   .select('#chart1')
@@ -17,18 +18,25 @@ const svg = d3
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+const keys = ['Valence', 'Danceability', 'Energy', 'Popularity']
+const colorScaleKeys = d3
+  .scaleOrdinal()
+  .domain(['Valence', 'Danceability', 'Energy', 'Popularity'])
+  .range(['#5CDB95', '#C7493A', '#dbc269', '#566fa3'])
+
 // Build your scales here
 const xPositionScale = d3
   .scaleLinear()
   .domain([0, 100])
   .range([0, width])
 
-const keys = ['Valence', 'Danceability', 'Energy', 'Popularity']
-
-const color = d3
+const yPositionScale = d3
   .scaleOrdinal()
-  .domain(keys)
-  .range(['#dbc269', '#5CDB95', '#C7493A', '#566fa3'])
+  .range([height * 0.3, height * 0.45, height * 0.6, height * 0.7])
+
+const colorScale = d3
+  .scaleOrdinal()
+  .range(['#566fa3', '#dbc269', '#C7493A', '#5CDB95'])
 
 d3.csv(require('/data/spotify_punk_playlists.csv'))
   .then(ready)
@@ -37,111 +45,72 @@ d3.csv(require('/data/spotify_punk_playlists.csv'))
   })
 
 function ready(datapoints) {
-  // console.log(datapoints)
   d3.select('i').on('click', d => {
     if (audio) {
       audio.pause()
     }
   })
 
-  const annotations = [
-    {
-      note: {
-        wrap: 140,
-        label: 'Green Day and Fall Out Boy are the cool kids, obviously'
-      },
-      type: d3Annotation.annotationCalloutRect,
-      subject: {
-        width: 25,
-        height: -25
-      },
-      data: { category: height - 207, song_popularity: 76 },
-      dx: xPositionScale(20),
-      dy: 0,
-      color: ['silver']
-    }
-  ]
+  // cleaning up the data
+  const valences = datapoints.map(d => ({
+    ...d,
+    category: 'valence',
+    amount: d.valence
+  }))
+  const danceabilities = datapoints.map(d => ({
+    ...d,
+    category: 'danceability',
+    amount: d.danceability
+  }))
+  const energies = datapoints.map(d => ({
+    ...d,
+    category: 'energy',
+    amount: d.energy
+  }))
+  const popularities = datapoints.map(d => ({
+    ...d,
+    category: 'popularity',
+    amount: d.song_popularity
+  }))
 
-  const makeAnnotations = d3Annotation
-    .annotation()
-    .accessors({
-      x: d => xPositionScale(d.song_popularity),
-      y: d => d.category
-    })
-    .annotations(annotations)
-    .notePadding(10)
-
-  // svg.call(makeAnnotations)
+  const merged = popularities.concat(energies, danceabilities, valences)
 
   svg
-    .selectAll('labels')
-    .data(keys)
-    .enter()
-    .append('text')
-    .text(d => d)
-    .attr('x', -20)
-    .attr('y', function(d, i) {
-      return height - 20 - i * 65
-    })
-    .style('fill', d => color(d))
-    .style('text-anchor', 'end')
+    .append('line')
+    .attr('class', 'even-line')
+    .attr('x1', width / 2)
+    .attr('x2', width / 2)
+    .attr('y1', 0)
+    .attr('y2', height)
+    .attr('stroke', 'whitesmoke')
+    .attr('stroke-dasharray', '10,20')
 
-  // energy
-  svg
-    .selectAll('circle')
-    .data(datapoints)
+  const graph = svg
+    .selectAll('categories')
+    .data(merged)
     .enter()
     .append('circle')
-    .attr('cy', height - 155)
-    .attr('id', 'energy')
-    .attr('cx', d => xPositionScale(d.energy))
-    .attr('fill', '#C7493A')
-    .attr('fill-opacity', 0.5)
-    .attr('r', 5)
+    .attr('cy', d => yPositionScale(d.category))
+    .attr('cx', d => xPositionScale(+d.amount))
+    .attr('fill', d => colorScale(d.category))
+    .attr('fill-opacity', 0.8)
+    .attr('r', 2)
     .attr('class', d => 'id-' + d.id)
+    .attr('id', d => 'column-' + d.category)
 
-  // popularity
-  svg
-    .selectAll('svg')
-    .data(datapoints)
-    .enter()
-    .append('circle')
-    .attr('cy', height - 220)
-    .attr('id', 'popularity')
-    .attr('cx', d => xPositionScale(d.song_popularity))
-    .attr('fill', '#566fa3')
-    .attr('fill-opacity', 0.5)
-    .attr('r', 5)
-    .attr('class', d => 'id-' + d.id)
+  // d3-force
+  const simulation = d3
+    .forceSimulation()
+    .force('x', d3.forceX(d => xPositionScale(+d.amount)).strength(0.6))
+    .force('y', d3.forceY(d => yPositionScale(d.category)).strength(0.2))
+    .force('charge', d3.forceManyBody().strength(-1.5))
+    .force('collide', d3.forceCollide(3))
 
-  // danceability
-  svg
-    .selectAll('svg')
-    .data(datapoints)
-    .enter()
-    .append('circle')
-    .attr('cy', height - 90)
-    .attr('id', 'danceability')
-    .attr('cx', d => xPositionScale(d.danceability))
-    .attr('fill', '#5CDB95')
-    .attr('fill-opacity', 0.5)
-    .attr('r', 5)
-    .attr('class', d => 'id-' + d.id)
+  function ticked() {
+    graph.attr('cx', d => d.x).attr('cy', d => d.y)
+  }
 
-  // valence
-  svg
-    .selectAll('svg')
-    .data(datapoints)
-    .enter()
-    .append('circle')
-    .attr('cy', height - 25)
-    .attr('id', 'valence')
-    .attr('cx', d => xPositionScale(d.valence))
-    .attr('fill', '#dbc269')
-    .attr('fill-opacity', 0.5)
-    .attr('r', 5)
-    .attr('class', d => 'id-' + d.id)
-
+  // audio and interactives
   svg
     .selectAll('circle')
     .on('mouseover', function(d) {
@@ -149,37 +118,20 @@ function ready(datapoints) {
         .text('')
         .append()
         .text(d.artist_name)
+        .style('font-family', 'Open Sans')
       d3.select('.songbox .song')
         .text('')
         .append()
         .text(d.song_name)
-      svg
-        .selectAll('.id-' + d.id)
-        .transition()
-        .duration(0)
-        .attr('r', 15)
-        .attr('fill-opacity', 0.7)
-      // .raise()
+        .style('font-family', 'Open Sans')
+      svg.selectAll('.id-' + d.id).attr('r', 15)
     })
     .on('mouseout', function() {
-      svg
-        .selectAll('circle')
-        .transition()
-        .duration(0)
-        .attr('r', 5)
-        .attr('fill-opacity', 0.5)
+      svg.selectAll('circle').attr('r', 2)
     })
     .on('click', function(d) {
-      svg
-        .selectAll('circle')
-        .attr('r', 5)
-        .attr('fill-opacity', 0.5)
-      svg
-        .selectAll('.id-' + d.id)
-        .transition()
-        .duration(0)
-        .attr('r', 15)
-        .attr('fill-opacity', 1)
+      svg.selectAll('circle').attr('r', 2)
+      svg.selectAll('.id-' + d.id).attr('r', 15)
 
       if (audio) {
         audio.pause()
@@ -188,15 +140,53 @@ function ready(datapoints) {
       return audio.play()
     })
 
-  const xAxis = d3
-    .axisBottom(xPositionScale)
-    .ticks(5)
-    .tickFormat(d => d + '%')
+  // text
   svg
-    .append('g')
-    .attr('class', 'axis x-axis axisWhite')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis)
+    .append('text')
+    .text('← less')
+    .attr('class', 'less')
+    .attr('x', xPositionScale(38))
+    .attr('y', -25)
+    .style('font-size', 18)
+    .attr('fill', 'whitesmoke')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+
+  svg
+    .append('text')
+    .text('50%')
+    .attr('class', 'even')
+    .attr('x', xPositionScale(50))
+    .attr('y', -25)
+    .style('font-size', 18)
+    .attr('fill', 'whitesmoke')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+
+  svg
+    .append('text')
+    .text('more →')
+    .attr('class', 'more')
+    .attr('x', xPositionScale(62))
+    .attr('y', -25)
+    .style('font-size', 18)
+    .attr('fill', 'whitesmoke')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+
+  // svg
+  //   .selectAll('labels')
+  //   .data(keys)
+  //   .enter()
+  //   .append('text')
+  //   .attr('class', 'labels')
+  //   .text(d => d)
+  //   .attr('y', -60)
+  //   .style('fill', d => colorScaleKeys(d))
+  //   .attr('text-anchor', 'middle')
+  //   .attr('alignment-baseline', 'middle')
+  //   .style('font-size', 16)
+  //   .style('font-weight', 600)
 
   function render() {
     const svgContainer = svg.node().closest('div')
@@ -205,63 +195,40 @@ function ready(datapoints) {
     const actualSvg = d3.select(svg.node().closest('svg'))
     actualSvg.attr('width', svgWidth)
 
-    const newWidth = svgWidth - margin.left - margin.right
+    const newWidth = svgWidth - margin.right - margin.left
 
     xPositionScale.range([0, newWidth])
 
-    if (svgWidth < 400) {
-      xAxis.ticks(2)
-    } else if (svgWidth < 650) {
-      xAxis.ticks(4)
+    // divider line
+    svg
+      .select('.even-line')
+      .attr('x1', newWidth / 2)
+      .attr('x2', newWidth / 2)
+
+    // svg.selectAll('.labels').attr('x', function(d, i) {
+    //   return newWidth / 2 + i * 50
+    // })
+
+    // circles
+    svg.selectAll('circle').attr('cx', d => xPositionScale(+d.amount))
+
+    // fix label positioning + run simulation if bigger
+    if (svgWidth > 550) {
+      // annotations
+      svg.select('.less').attr('x', xPositionScale(38))
+      svg.select('.even').attr('x', xPositionScale(50))
+      svg.select('.more').attr('x', xPositionScale(62))
+
+      // the force is with me
+      simulation.nodes(merged).on('tick', ticked)
     } else {
-      xAxis.ticks(null)
+      // annotations
+      svg.select('.less').attr('x', xPositionScale(20))
+      svg.select('.even').attr('x', xPositionScale(50))
+      svg.select('.more').attr('x', xPositionScale(80))
     }
-
-    xAxis.ticks(svgWidth / 200)
-
-    svg.select('.x-axis').call(xAxis)
-
-    svg.selectAll('#energy').attr('cx', d => xPositionScale(d.energy))
-    svg
-      .selectAll('#popularity')
-      .attr('cx', d => xPositionScale(d.song_popularity))
-    svg.selectAll('#valence').attr('cx', d => xPositionScale(d.valence))
-    svg
-      .selectAll('#danceability')
-      .attr('cx', d => xPositionScale(d.danceability))
-
-    const annotations = [
-      {
-        note: {
-          wrap: 140,
-          label: 'Green Day and Fall Out Boy are the cool kids, obviously'
-        },
-        type: d3Annotation.annotationCalloutRect,
-        subject: {
-          width: 35,
-          height: -25
-        },
-        data: { category: height - 207, song_popularity: 76 },
-        dx: newWidth / 3.3,
-        dy: 0,
-        color: ['silver']
-      }
-    ]
-
-    const makeAnnotations = d3Annotation
-      .annotation()
-      .accessors({
-        x: d => xPositionScale(d.song_popularity),
-        y: d => d.category
-      })
-      .annotations(annotations)
-      .notePadding(10)
-
-    // svg.call(makeAnnotations)
   }
 
-  // When the window resizes, run the function that redraws everything
   d3.select(window).on('resize', render)
-
   render()
 }
